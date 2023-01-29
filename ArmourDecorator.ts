0b46d99f92b8a3c4ced4f079dfb8fb81bfd53be5
CamiloMM21 <121126131+CamiloMM21@users.noreply.github.com>
2023-01-29 01:14:32 -0500

    Delete ArmourDecorator.ts

--- ArmourDecorator.ts
+++
@@ -1,6 +0,0 @@
-import EnemyDeorator from "./EnemyDecorator";
-export class ArmourDecorator extends EnemyDeorator{
-    takeDamage(): number {
-        return this.enemy.takeDamage() / 1.5;
-    }
-}
