26996f935565994684f0ff63a3e305d508df9d04
CamiloMM21 <121126131+CamiloMM21@users.noreply.github.com>
2023-01-28 23:33:33 -0500

    Delete main.ts

--- main.ts
+++
@@ -1,8 +0,0 @@
-import { ArmourDecorator } from "./ArmourDecorator";
-import BaseEnemy from "./BaseEnemy";
-import { HelmetDecorator } from "./HelmetDecorator";
-let enemy = new BaseEnemy();
-let EnemywithHelmet =new HelmetDecorator(enemy);
-let EnemywithHelmetAndArmour = new ArmourDecorator(EnemywithHelmet)
-let comutedDamaged = EnemywithHelmet.takeDamage();
-console.log(comutedDamaged);
